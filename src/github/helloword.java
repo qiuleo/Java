6008672c63ca7768a0325cd08e073cc85cf7dd44
Qiuleo <459226876@qq.com>
2016-01-07 16:14:09 +0800

    no message

---
+++ src/github/helloword.java
@@ -0,0 +1,9 @@
+package github;
+import java.util.*;
+
+public class helloword {
+	public static void main(String[] args){
+		System.out.println("Hello World");
+		System.out.println(new Date());
+	}
+}
